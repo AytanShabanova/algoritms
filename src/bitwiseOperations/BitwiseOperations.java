34aa2bd78c7f7c302d1ac6d51fdc9a916c52bbd9
AytanShabanova <you@example.com>
2024-07-14 06:04:33 -0700

    add algoritms

---
+++ src/bitwiseOperations/BitwiseOperations.java
@@ -0,0 +1,20 @@
+package bitwiseOperations;
+
+public class BitwiseOperations {
+    public static void main(String[] args) {
+        System.out.println(51 & 25); //17
+        System.out.println(51 |25); //59
+        System.out.println(51 ^ 25); //42
+        System.out.println(~23); //-24
+        System.out.println(12 << 2);//48
+        System.out.println(12 >> 2);//3
+
+
+
+
+
+
+
+
+    }
+}
